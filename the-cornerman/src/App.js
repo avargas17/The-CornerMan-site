56ef9aba696d71453acd86f4f986068fe8d06811
avargas17 <73358723+avargas17@users.noreply.github.com>
2023-04-23 14:48:35 -0500

    Creating a simple Hero Section
    Created a simple hero section, the css needs some work to better incorporate an image.

--- the-cornerman/src/App.js
+++ the-cornerman/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar';
+import Hero from "./components/HeroSection"
 
 class App extends Component {
   render() {
     return (
       <div>
         <Navbar />
+        <Hero/>
         <h1>Welcome to my React App!</h1>
       </div>
     );
---
+++ the-cornerman/src/components/HeroSection.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./HeroSection.css"
+
+function Hero() {
+  return (
+    <section className="hero">
+    {/*<img src="/images/MvsA.jpeg" alt="Fight" />*/}
+      <h1>Upcoming Events</h1>
+      <p>Get the best MMA news in the business from your CornerMan</p>
+      <button>Get Started</button>
+    </section>
+  );
+}
+
+export default Hero;
