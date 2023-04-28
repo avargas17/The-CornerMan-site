1dc1802602c3967285ec96ea8d685791a35d402b
avargas17 <73358723+avargas17@users.noreply.github.com>
2023-04-28 11:12:51 -0500

    Changes to image in hero section
    Trying to make it staionary so it doesn't scroll with the page.

--- the-cornerman/src/components/HeroSection.js
+++ the-cornerman/src/components/HeroSection.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import "./HeroSection.css"
-import Fight from '../images/RDA.jpg'
+
 function Hero() {
   return (
     <section className="hero">
-    <img src={Fight} alt="walking shoes" autoPlay loop muted/>
       <h1>The Fight News</h1>
       <p>Get the best MMA news in the business from your CornerMan</p>
       <button>Get Started</button>
